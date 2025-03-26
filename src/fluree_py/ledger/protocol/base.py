75cd5682c1d01edceee5e7a299f7d03bf90b284e
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-03-26 18:09:00 -0400

    refactor: pulled out BaseBuilder and BaseReadyToCommit

---
+++ src/fluree_py/ledger/protocol/base.py
@@ -0,0 +1,13 @@
+from typing import Protocol
+
+from fluree_py.ledger.protocol.commit import SupportsCommitable
+from fluree_py.ledger.protocol.context import SupportsContext
+from fluree_py.ledger.protocol.request import SupportsRequestCreation
+
+
+class BaseBuilder(SupportsContext, Protocol):
+    """Base protocol for all builders that support context."""
+
+
+class BaseReadyToCommit(SupportsRequestCreation, SupportsCommitable, Protocol):
+    """Base protocol for all builders that are ready to commit."""
