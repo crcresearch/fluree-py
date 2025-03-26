19426b06dcc25093b4f133d3afb1bc9bfa60c51d
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-03-26 18:09:00 -0400

    refactor: pulled out BaseBuilder and BaseReadyToCommit

--- src/fluree_py/ledger/protocol/transaction.py
+++ src/fluree_py/ledger/protocol/transaction.py
@@ -1,19 +1,19 @@
 from typing import Any, Protocol
 
-from fluree_py.ledger.protocol.commit import SupportsCommitable
-from fluree_py.ledger.protocol.context import SupportsContext
-from fluree_py.ledger.protocol.request import SupportsRequestCreation
+from fluree_py.ledger.protocol.base import BaseBuilder, BaseReadyToCommit
 
 
-class TransactionBuilder(SupportsContext, Protocol):
+class TransactionBuilder(BaseBuilder, Protocol):
+    """Protocol for transaction builders."""
+
     def with_insert(self, data: dict[str, Any]) -> "TransactionReadyToCommit": ...
     def with_delete(self, data: dict[str, Any]) -> "TransactionReadyToCommit": ...
     def with_where(self, clause: dict[str, Any]) -> "TransactionBuilder": ...
 
 
-class TransactionReadyToCommit(
-    SupportsContext, SupportsRequestCreation, SupportsCommitable, Protocol
-):
+class TransactionReadyToCommit(BaseBuilder, BaseReadyToCommit, Protocol):
+    """Protocol for transaction builders that are ready to commit."""
+
     def with_insert(self, data: dict[str, Any]) -> "TransactionReadyToCommit": ...
     def with_delete(self, data: dict[str, Any]) -> "TransactionReadyToCommit": ...
     def with_where(self, clause: dict[str, Any]) -> "TransactionReadyToCommit": ...
