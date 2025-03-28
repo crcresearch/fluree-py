1d7f4da789e44be3b94e749f556dad958a822c29
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-03-27 20:18:21 -0400

    test: move custom option to run tests against a real server to the

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,21 @@ from testcontainers.generic import ServerContainer
 from fluree_py import FlureeClient
 
 
+def pytest_addoption(parser):
+    """Add custom command line options."""
+    parser.addoption(
+        "--use-fluree-server",
+        action="store_true",
+        default=False,
+        help="Run tests against a real Fluree server container",
+    )
+
 @pytest.fixture(scope="session")
-def fluree_url() -> Generator[str, None, None]:
+def fluree_url(request) -> Generator[str | None, None, None]:
+    if not request.config.getoption("--use-fluree-server"):
+        yield None
+        return
+    
     container = ServerContainer(port=8090, image="fluree/server")
     container.start()
 
@@ -26,7 +39,9 @@ def fluree_url() -> Generator[str, None, None]:
 
 
 @pytest.fixture(scope="session")
-def fluree_client(fluree_url):
+def fluree_client(fluree_url) -> FlureeClient | None:
+    if fluree_url is None:
+        return None
     return FlureeClient(base_url=fluree_url)
 
 
