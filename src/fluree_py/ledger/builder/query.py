6f5b11347149f2edfd0cf361ae6f745c17dccba6
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-03-29 15:51:04 -0400

    feat(typing): introduce remaining types for query

--- src/fluree_py/ledger/builder/query.py
+++ src/fluree_py/ledger/builder/query.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, replace
 from typing import Any, Self
 
 from fluree_py.ledger.mixin import CommitableMixin, RequestMixin, WithContextMixin
-from fluree_py.ledger.protocol.query import QueryBuilder, ActiveIdentity
+from fluree_py.ledger.protocol.query import GroupByClause, HavingClause, OrderByClause, QueryBuilder, ActiveIdentity
 from fluree_py.query.select.types import SelectArray, SelectObject
 from fluree_py.query.where.types import WhereClause
 
@@ -12,22 +12,22 @@ class QueryBuilderImpl(QueryBuilder, RequestMixin, WithContextMixin, CommitableM
     endpoint: str
     ledger: str
     where: WhereClause | None = None
-    group_by: list[str] | None = None
-    having: dict[str, Any] | None = None
-    order_by: list[str] | None = None
+    group_by: GroupByClause | None = None
+    having: HavingClause | None = None
+    order_by: OrderByClause | None = None
     opts: ActiveIdentity | None = None
     select_fields: dict[str, Any] | list[str] | None = None
 
     def with_where(self, conditions: WhereClause) -> Self:
         return replace(self, where=conditions)
 
-    def with_group_by(self, fields: list[str]) -> Self:
+    def with_group_by(self, fields: GroupByClause) -> Self:
         return replace(self, group_by=fields)
 
-    def with_having(self, condition: dict[str, Any]) -> Self:
+    def with_having(self, condition: HavingClause) -> Self:
         return replace(self, having=condition)
 
-    def with_order_by(self, fields: list[str]) -> Self:
+    def with_order_by(self, fields: OrderByClause) -> Self:
         return replace(self, order_by=fields)
 
     def with_opts(self, opts: ActiveIdentity) -> Self:
