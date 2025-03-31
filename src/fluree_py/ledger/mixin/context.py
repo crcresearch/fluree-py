90595dcb0d746e3acfac665b96fe35098b9ef91b
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-03-31 12:30:45 -0400

    fix: remove debug printouts

--- src/fluree_py/ledger/mixin/context.py
+++ src/fluree_py/ledger/mixin/context.py
@@ -6,21 +6,9 @@ from fluree_py.ledger.protocol.mixin import HasContextData
 T = TypeVar("T", bound="HasContextData")
 
 
-def print_special_vars(obj: Any) -> None:
-    all_attributes = dir(obj)
-    special_vars = [
-        attr for attr in all_attributes if attr.startswith("__") and attr.endswith("__")
-    ]
-    print(f"Special vars: {special_vars}")
-    for var in special_vars:
-        print(f"\t{var}: {getattr(obj, var)}")
-
-
 class WithContextMixin(Generic[T]):
     def with_context(self, context: dict[str, Any]) -> T:
-        print_special_vars(self.__class__)
         resolved_type = resolve_base_class_reference(self.__class__, "WithContextMixin")
-        print(resolved_type)
 
         # Manually create a new instance with updated context
         updated_fields = self.__dict__.copy()
